f4966856a3a1baad7a93ee2394d59c78e99c124f
DJ Martin <umdjm@hotmail.com>
2012-10-03 11:22:21 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MAXMarketing::Application.config.secret_token = '008288302ce05773b80774b9a19a6e415f23cb12ac13b8a79df6e423926b60014375fb779cb167958626d42d6b58dbb4b471e4d3726056501c2b051939d9c39c'
